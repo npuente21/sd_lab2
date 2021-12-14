6082279e492291221a865f2c2392d16329e2aa56
lucasdauc <>
2021-12-14 15:34:17 -0300

    local to machines

--- Informante/main.go
+++ Informante/main.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	//address = "10.6.40.183:50000"
-	address = "localhost:50000"
+	address = "10.6.40.183:50000"
+	//address = "localhost:50000"
 )
 
 var informante = []string{"AHSOKA TANO", "ALMIRANTE THRAWN"}
